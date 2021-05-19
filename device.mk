bfc8077dd41c143509d1059f02fa28967fcf25ac
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2021-05-19 10:32:12 +0300

    guacamoleb: Build new OnePlus camera HALS
    Co-authored-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- device.mk
+++ device.mk
@@ -168,11 +168,9 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64 \
     libxml2 \
-    vendor.oneplus.camera.CameraHIDL@1.0.vendor \
-    vendor.qti.hardware.camera.device@1.0.vendor
+    vendor.oneplus.hardware.camera@1.0.vendor:64 \
+    vendor.qti.hardware.camera.device@1.0.vendor:64
 
 # Init scripts
 PRODUCT_PACKAGES += \
