f94136195832a100ed89d91850fe4a463406f7f4
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2021-06-17 18:55:40 +0300

    guacamoleb: init: Set ro.vendor.serialno

--- init/init_guacamoleb.cpp
+++ init/init_guacamoleb.cpp
@@ -93,4 +93,7 @@ void vendor_load_properties()
 
     property_override("vendor.boot.prj_version", std::to_string(prj_version).c_str());
     property_override_dual("vendor.rf.version", "vendor.boot.rf_version", std::to_string(rf_version).c_str());
+
+    std::string serialno = android::base::GetProperty("ro.serialno", "");
+    property_override("ro.vendor.serialno", serialno.c_str());
 }
