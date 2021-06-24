e8d63eed58747dafec542cf3047ce1b01140386f
baalajimaestro <me@baalajimaestro.me>
2021-06-24 10:34:07 +0530

    Drop signing keys
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>

--- device.mk
+++ device.mk
@@ -3,8 +3,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-# Release key
-PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/hnt/release-keys/releasekey
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
