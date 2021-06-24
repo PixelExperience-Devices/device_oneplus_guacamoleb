d92c8c6d013b2243323a8e9aa4875e733b901c93
baalajimaestro <me@baalajimaestro.me>
2021-06-24 12:31:35 +0530

    Bringup PE
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>

--- aosp_guacamoleb.mk
+++ aosp_guacamoleb.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/guacamoleb/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_guacamoleb
+PRODUCT_NAME := aosp_guacamoleb
 PRODUCT_DEVICE := guacamoleb
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := GM1905
