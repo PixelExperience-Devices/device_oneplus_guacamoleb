d6d80ec79620406fb759609a1a21e3415eb0cf1b
baalajimaestro <me@baalajimaestro.me>
2021-06-24 12:39:48 +0530

    Use ld.lld for kernel
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,7 +40,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8150
 TARGET_KERNEL_CONFIG := sm8150_defconfig
-TARGET_KERNEL_ADDITIONAL_FLAGS := AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
+TARGET_KERNEL_ADDITIONAL_FLAGS := AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LD=ld.lld
 TARGET_KERNEL_CLANG_VERSION := proton
 
 # Platform
