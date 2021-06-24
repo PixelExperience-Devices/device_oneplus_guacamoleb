41b8a52e111753bc60c38ea52484a009b8c12149
baalajimaestro <me@baalajimaestro.me>
2021-06-24 21:46:41 +0530

    Build overlays
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>

--- device.mk
+++ device.mk
@@ -22,11 +22,9 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-rro-excluded
 
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
