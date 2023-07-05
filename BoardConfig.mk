46b0f40406cf9c2e2cb2dfe682960b65ea8409b4
Shantanu Sarkar <shantanuplussarkar@gmail.com>
2023-07-05 10:42:51 +0530

    guacamoleb: Update blobs from OOS 11.0.9.1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -144,7 +144,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security
-VENDOR_SECURITY_PATCH := 2022-02-01
+VENDOR_SECURITY_PATCH := 2022-06-01
 
 # SELinux
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
